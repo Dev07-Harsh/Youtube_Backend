4a02d05f595f829027b527d09ffe5b84e14e3a36
Dev07-Harsh <150646921+Dev07-Harsh@users.noreply.github.com>
2025-01-06 12:06:51 +0530

    All setup configured

--- src/index.js
+++ src/index.js
@@ -1,33 +1,21 @@
 import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+import {app} from './app.js'
 dotenv.config({
-    path : './env'
+    path: './.env'
 })
 
-import connectDB from "./db/index.js";
 
 
 connectDB()
-.then(()=>{
-    app.on("error",(error)=>{
-        console.log("Error:",error);
-        throw error
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
     })
-    
-    app.listen(process.env.PORT || 8000,()=>{
-        console.log("Server is running on port 8000");
-     })
-
 })
-
 .catch((err) => {
-    console.log("Mongo db connection failed !",err);
+    console.log("MONGO db connection failed !!! ", err);
 })
-
-
-
-
-
-
 
 
 /*
